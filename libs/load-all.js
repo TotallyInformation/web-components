812a070208f82a996181909ea53c45078f77bcec
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2022-05-16 22:34:29 +0100

    Shortcut component that loads all available components

---
+++ libs/load-all.js
@@ -0,0 +1,18 @@
+/** Simple ECMA module to load uibuilder module and all of the web component modules
+ * Assumes you are using Node-RED/uibuilder
+ */
+
+import './uibuilder.module.js'  // Adds `uibuilder` and `$` to globals
+
+import '../components/button-send.js'
+import '../components/container-br.js'
+import '../components/data-list.js'
+import '../components/definition-list.js'
+import '../components/html-include.js'
+import '../components/labelled-value.js'
+import '../components/simple-card.js'
+import '../components/simple-container.js'
+import '../components/syntax-highlight.js'
+import '../components/uib-theme-changer.js'
+
+export default {}
