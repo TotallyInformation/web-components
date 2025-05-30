5bb7da23195f2fef08a8250c0e7680af77aa2d1b
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2025-05-30 15:05:51 +0100

    promote data-list from alpha to live

--- docs/components/data-list.md
+++ docs/components/data-list.md
@@ -3,80 +3,166 @@ title: data-list
 description: >
   A Zero dependency web component lets you create a ul or ol list from a JavaScript object or array.
 created: 2022-04-24 16:57:57
-lastUpdated: 2025-05-29 20:32:10
-status: alpha # alpha, beta, live
+lastUpdated: 2025-05-30 13:44:55
+author: Julian Knight (Totally Information)
+status: live # alpha, beta, live
 ---
 
-Contains relevant data from data-*, topic and payload attributes (or properties),
-includes a _meta object showing whether any modifier keys were used, the element id/name
-
-## Usage
-
-You can use this with or without Node-RED and uibuilder but it is most use when used with uibuilder.
-
-When used without uibuilder, you need to detect the custom event that is fired on the document object.
-
-### Without uibuilder
-
-#### index.html
+## Useage
 
 ```html
-<!doctype html>
-<html lang="en"><head>
+<data-list></data-list>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- override bullet type -->
+<data-list id="dl1" style="--list-style: disclosure-closed">
+    Items added by JavaScript
+</data-list>
 
-    <!-- Can load the web components here as type=module but you have to load everything in the right order.
-         Alternatively, use an import in index.js -->
-    <!-- <script type="module" async src="https://cdn.jsdelivr.net/gh/totallyinformation/web-components@main/components/button-send.js"></script> -->
+<!-- Using object input, set to ol, override separator between key and value -->
+<data-list id="dl2" listvar="mylist1" type="ol" keyvalueseparator=" - ">
+    Items added by global object variable
+</data-list>
 
-    <script type="module" async src="./index.js"></script> 
+<!-- Using array input, override list style (don't forget to wrap custom entries with quotes) -->
+<data-list id="dl3" listvar="mylist2" liststyle="'➡️'">
+    Items added by global array variable
+</data-list>
 
-</head><body>
-
-<div>
-    <!-- The simplest use. Apply the data in your index.js. Give it an ID to make it easier to reference. -->
-    <data-list id="dl1"></data-list>
-
-    <!-- If you give it a name, it will be used as its ID but with spaces replace with underscores. -->
-    <!-- <data-list name="jims list">Jim's Simple List</data-list> -->
-
-    <!-- If you don't give an ID or name, the component will apply an ID for you. -->
-    <!-- <data-list>Simple List</data-list> -->
-</div>
-
-</body></html>
+<!-- Override decimal list with upper case roman numerals -->
+<data-list type="ol" liststyle="upper-roman"></data-list>
 ```
 
-#### index.js
+## Attributes & Properties
 
-```js
-/** Main app script
- * NOTES: 
- * - Any imports are relative to THIS script.
- */
+### Attributes
 
-// Load the component. The component self-registers & adds the `DataList` object to the `window` global.
-import '../uibuilder/vendor/@totallyinformation/web-components/components/data-list.js'
+All of the attributes are optional.
 
-// Get a reference to your data-list tag
-const dl1 = document.querySelector('#dl1')
+* Attributes specific to this component
 
-// Initialise the data-list.
-dl1.data = {
-    "one": "This is the first list entry",
-    "two": "This is the second list entry",
-    "three": "This is the third list entry",
-}
+  * `keyvalueseparator` - OPTIONAL. The separator to use between the key and value when input is an object rather than an array. Defaults to `" :: "`. Set to `"NULL"` to disable display of the keys.
+  * 💫`listvar` - OPTIONAL. The name of a global variable to use to generate the list. If not specified, the component will use the `data` property which you will need to set directly using JavaScript. No default.
+  * 💫`listStyle` - OPTIONAL. The style type to use for the list. Default is `disc` for `ul` and `decimal` for `ol`. May contain any valid CSS list-style string value. When set, will change the `--list-style` CSS variable.
+  * 💫`type` - OPTIONAL. The type of list to create. Either `ul` (unordered) or `ol` (ordered). Defaults to `ul`.
 
-// Change one of the entries in the list after 4 seconds
-setTimeout(function(){
+  > [!TIP]
+  > `listStyle` entries must be wrapped in extra quotes if they are not keywords, e.g. `liststyle="'➡️'"` or `liststyle="decimal"`. This is because the value is a CSS variable and must be a valid CSS string.
 
-    dl1.entry('two', 'This is the second entry amended')
+* Attributes inherited from the `BaseComponent`:
 
-}, 4000)
-```
+  * `inherit-style` - OPTIONAL. Load external styles into component. If present but empty, will default to './index.css'. Optionally give a URL to load. No default.
+  * `name` - HTML name attribute. Included in output _meta prop.
+
+> [!NOTE]
+> The 💫 symbol indicates that the attribute is dynamic. If changed by JavaScript, the list will be rebuilt. Technically, this means that the property has a getter and setter function.
+
+### Properties
+
+Each attribute has a corresponding property. You can set the property directly in JavaScript or by using the `setAttribute` method. Note that setting the property will not change the attribute
+
+* Properties specific to this component (that don't have a corresponding attribute):
+
+  * {object|array} 💫`data` - The data to use to generate the list. This can be an array or an object. If an object, the keys and values will be used as the list items' text separated by the `keyvalueseparator` string. If an array, each item in the array will be used as a list item. If no data is provided, the component will not display a list, only the slot content. If `listvar` is specified, the component will use that global variable to generate the list (the variable must be attached to the `window`/`globalThis` DOM object).
+
+* Properties inherited from the `BaseComponent`:
+
+  * {number} `_iCount` Static. The count of instances of this component that weren't given an id. Creates a unique id as needed.
+  * {boolean} `uib` True if UIBUILDER for Node-RED is loaded. (Default=FALSE)
+  * {object} `uibuilder` Reference to loaded UIBUILDER for Node-RED client library if loaded (else undefined).
+  * {function(string): Element} `$` jQuery-like shadow dom selector (or undefined if shadow dom not used).
+  * {function(string): NodeList} `$$`  jQuery-like shadow dom multi-selector (or undefined if shadow dom not used).
+  * {boolean} `connected` False until connectedCallback finishes.
+  * {string} `name` Placeholder for the optional name attribute.
+  * {object} `opts` This components controllable options - get/set using the `config()` method - empty object by default.
+  * {string} `baseVersion` Static. The base component version string (date updated).
+
+* Other standard properties:
+
+  * `{string} componentVersion` Static. The component version string (date updated). Also has a getter that returns component and base version strings.
+
+> [!NOTE]
+> Static properties have to be accessed via the component's classname. e.g. `DataList.componentVersion`
+
+## Slots
+
+The component has a main slot that can be used to provide additional content. The slot content will be displayed above the list.
+
+> [!NOTE]
+> Slots are used to allow the component to accept HTML content. By default, the main slot, if defined, will contain any content between the opening and closing tags of the component. You can also define named slots to allow for more complex content arrangements.
+
+## Styling
+
+### Inheriting Styles
+
+This component uses the Shadow DOM to encapsulate its styles. This means that the component will not inherit styles from the main document. However, you can still style the component using [CSS variables](https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties). You can also use the `inherit-styles` attribute to allow the component to inherit styles from a style link (usually the same as the main page's style sheet).
+
+### CSS Variables
+
+The following CSS variables are available for styling:
+
+* `--list-style` - Maps directly to the `list-style` CSS property. Defaults to `disc` for unordered lists and `decimal` for ordered lists. This is set by the `listStyle` attribute. However, you can override it in your own stylesheets and/or HTML if you wish.
+
+## Events
+
+The standard events for this library of components are also available:
+
+* `data-list:connected` - When an instance of the component is attached to the DOM. `evt.details` contains the details of the element.
+* `data-list:ready` - Alias for connected. The instance can handle property & attribute changes
+* `data-list:disconnected` - When an instance of the component is removed from the DOM. `evt.details` contains the details of the element.
+* `data-list:attribChanged` - When a watched attribute changes. `evt.details.data` contains the details of the change.
+
+NOTE that event listeners can be attached either to the `document` or to the specific element instance. Use `document.addEventListener` if your web app might be adding new instances of the component dynamically.
+
+All events contain `evt.details.id` and `evt.details.name`. Some events contain `evt.details.data` if indicated.
+
+## Methods
+
+These methods can be used from JavaScript by first getting a reference to the appropriate element instance. As shown in the examples.
+
+* `entry(key, value)` - Add or update an entry in the list. If the key already exists, the value will be updated. If the key does not exist, a new entry will be added.  `key` should be a string for object data and an integer number for arrays.
+
+## Extensions for UIBUILDER for Node-RED
+
+### Browser to Node-RED
+
+None.
+
+### Node-RED to Browser
+
+None.
+
+#### Examples
+
+
+
+## Original Requirements
+
+* [x] Set a data object/array from which to build a `ul` or `ol` list.
+* [x] Have an attribute that defines a global variable that will set the data.
+* [x] Allow for both `ul` and `ol` lists via an attribute.
+* [x] Allow list text to include HTML formatting.
+* [x] Allow for object input data to display both the key and value in the list text. With the ability to turn off the key display.
+* [x] Allow for a key/value separator to be set via an attribute.
+* [x] Allow for the list style to be set via an attribute.
+* [x] Allow for the list to be styled via CSS variables.
+
+## Nice-to-have features
+
+Possibly in future versions.
+
+* [ ] Allow direct update of data from a uibuilder msg.
+
+
+## Design References
+
+* None.
+
+
+
+
+
+
+
 
 ### With uibuilder
 
@@ -159,9 +245,3 @@ uibuilder.onTopic('update dl1', function(msg) {
     }
 */
 ```
-
-----
-
-The remainder of this documentation is produced using [web-component-analyzer](https://github.com/runem/web-component-analyzer#-how-to-document-your-components-using-jsdoc)
-
-TBC
---
+++ src/data-list.js
@@ -0,0 +1,280 @@
+/** Define a new zero dependency custom web component ECMA module that can be used as an HTML tag
+ *
+ * Version: See the class code
+ *
+ */
+/** Copyright (c) 2022-2025 Julian Knight (Totally Information)
+ * https://it.knightnet.org.uk, https://github.com/TotallyInformation
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import TiBaseComponent from '../libs/ti-base-component'
+
+/**
+ * TODO: Add optional footer ?
+ *       topic (uib)
+ */
+
+/** Only use a template if you want to isolate the code and CSS */
+const template = document.createElement('template')
+template.innerHTML = /*html*/`
+    <style>
+        :host {
+            display: block;   /* default is inline */
+            contain: content; /* performance boost */
+            --list-style: disc; /* Default list style type */
+        }
+        ul {
+            list-style: var(--list-style, disc);
+        }
+    </style>
+    <slot></slot>
+    <ul></ul>
+`
+
+/** Namespace
+ * @namespace Live
+ */
+
+/**
+ * @class
+ * @augments TiBaseComponent
+ * @description Define a new zero dependency custom web component ECMA module that can be used as an HTML tag
+ *
+ * @element data-list
+ * @memberOf Live
+
+ * METHODS FROM BASE: (see TiBaseComponent)
+ * STANDARD METHODS:
+  * @function attributeChangedCallback Called when an attribute is added, removed, updated or replaced
+  * @function connectedCallback Called when the element is added to a document
+  * @function constructor Construct the component
+  * @function disconnectedCallback Called when the element is removed from a document
+
+ * OTHER METHODS:
+  * @function entry Dynamically ament/add a single entry. The list is rebuilt.
+
+ * CUSTOM EVENTS:
+  * "data-list:connected" - When an instance of the component is attached to the DOM. `evt.details` contains the details of the element.
+  * "data-list:ready" - Alias for connected. The instance can handle property & attribute changes
+  * "data-list:disconnected" - When an instance of the component is removed from the DOM. `evt.details` contains the details of the element.
+  * "data-list:attribChanged" - When a watched attribute changes. `evt.details.data` contains the details of the change.
+  * NOTE that listeners can be attached either to the `document` or to the specific element instance.
+
+ * Standard watched attributes (common across all my components):
+  * @property {string|boolean} inherit-style - Optional. Load external styles into component (only useful if using template). If present but empty, will default to './index.css'. Optionally give a URL to load.
+  * @property {string} name - Optional. HTML name attribute. Included in output _meta prop.
+
+ * Other watched attributes:
+  * @property {string} type - 💫 Optional. The type of list to use, ul or ol. Default is ul.
+  * @property {string} listvar - 💫 Optional. The global variable name to use for the list data. If not set, set the data property directly from JS.
+  * @property {string} keyvalueseparator - Optional. The separator to use between key and value in the list items when input is an object. Default is ' :: '. Set to "NULL" to disable key display.
+  * @property {string} liststyle - 💫 Optional. The style type to use for the list. Default is `disc` for `ul` and `decimal` for `ol`. May contain any valid CSS list-style string value.
+
+ * PROPS FROM BASE: (see TiBaseComponent)
+ * OTHER STANDARD PROPS:
+  * @property {string} componentVersion Static. The component version string (date updated). Also has a getter that returns component and base version strings.
+
+ * Other props:
+  * By default, all attributes are also created as properties
+  * @property {object} data - 💫 The data to use for the list. Either set directly or via the `listvar` attribute. If an object, the key/value separator is used to separate the key and value in the list items.
+
+ NB: properties marked with 💫 are dynamic and have getters/setters. They will cause the list to rebuild.
+
+ * @slot Container contents
+
+ * @example
+  * <data-list id="myComponent" type="ol" inherit-style="./myComponent.css"></data-list>
+
+ * See https://github.com/runem/web-component-analyzer?tab=readme-ov-file#-how-to-document-your-components-using-jsdoc
+ */
+class DataList extends TiBaseComponent {
+    /** Component version */
+    static componentVersion = '2025-05-30'
+
+    #list = null // The list element
+    #listStyle = 'disc' // The default list style type
+    #entries = null // The entries in the list
+    #type = 'ul' // The type of list to use, ul or ol
+
+    // Optional global variable name to use for the list data
+    listvar = null
+    // The separator to use between key and value in the list items
+    keyvalueseparator = ' :: '
+
+    /** Makes HTML attribute change watched
+     * @returns {Array<string>} List of all of the html attribs (props) listened to
+     */
+    static get observedAttributes() {
+        return [
+            // Standard watched attributes:
+            'inherit-style', 'name',
+            // Other watched attributes:
+            'keyvalueseparator', 'liststyle', 'listvar', 'type',
+        ]
+    }
+
+    /** NB: Attributes not available here - use connectedCallback to reference */
+    constructor() {
+        super()
+        // Only attach the shadow dom if code and style isolation is needed - comment out if shadow dom not required
+        if (template && template.content) this._construct(template.content.cloneNode(true))
+
+        // this.#list = document.createElement('ul')
+        this.#list = this.shadowRoot.querySelector('ul')
+    }
+
+    /** Runs when an instance is added to the DOM
+     * Runs AFTER the initial attributeChangedCallback's
+     */
+    connectedCallback() {
+        // console.info(`[DataList] ${this.id} connectedCallback (${this.tagName})`)
+        this._connect() // Keep at start.
+
+        if ( this.listvar && window[this.listvar] ) {
+            if ( window[this.listvar] ) {
+                this.data = window[this.listvar]
+                // window.dataList.instances[this.id] = window[newVal]
+                // console.log('>> listvar change >>', this.id, this.#entries, window.dataList.instances[this.id])
+                //window[newVal] = new Proxy()
+            } else {
+                console.warn(`[DataList] ${this.id} window.${this.listvar} does not exist, ignoring.`)
+            }
+
+            return
+        }
+
+        this._ready() // Keep at end. Let everyone know that a new instance of the component has been connected & is ready
+    }
+
+    /** Runs when an instance is removed from the DOM */
+    disconnectedCallback() {
+        this._disconnect() // Keep at end.
+    }
+
+    /** Runs when an observed attribute changes - Note: values are always strings
+     * NOTE: On initial startup, this is called for each watched attrib set in HTML.
+     *       and BEFORE connectedCallback is called.
+     * @param {string} attrib Name of watched attribute that has changed
+     * @param {string} oldVal The previous attribute value
+     * @param {string} newVal The new attribute value
+     */
+    attributeChangedCallback(attrib, oldVal, newVal) {
+        // console.info(`[DataList] ${this.id} attributeChangedCallback: ${attrib} changed from "${oldVal}" to "${newVal}"`)
+        /** Optionally ignore attrib changes until instance is fully connected
+         * Otherwise this can fire BEFORE everthing is fully connected.
+         */
+        // if (!this.connected) return
+
+        // Don't bother if the new value same as old
+        if ( oldVal === newVal ) return
+        // Create a property from the value - WARN: Be careful with name clashes
+        this[attrib] = newVal
+
+        // Add other dynamic attribute processing here.
+        // If attribute processing doesn't need to be dynamic, process in connectedCallback as that happens earlier in the lifecycle
+
+        // Keep at end. Let everyone know that an attribute has changed for this instance of the component
+        this._event('attribChanged', { attribute: attrib, newVal: newVal, oldVal: oldVal, })
+    }
+
+    // Getter/setter for the `type` public property - maps to this.#type
+    set type(val) {
+        if (!['ul', 'ol'].includes(val)) {
+            console.warn(`[DataList] ${this.id} Invalid type "${val}" specified, defaulting to "ul".`)
+            val = 'ul'
+        }
+
+        this.#type = val
+
+        // Update CSS classes/attributes instead of recreating element
+        this.#list.setAttribute('data-list-type', val)
+
+        // Update ARIA attributes for accessibility
+        if (val === 'ol') {
+            this.listStyle = 'decimal'
+            this.#list.setAttribute('aria-label', 'Ordered list')
+        } else {
+            this.listStyle = 'disc'
+            this.#list.setAttribute('aria-label', 'Unordered list')
+        }
+    }
+    get type() {
+        return this.#type
+    }
+
+    // Getter/setter for the `listStyle` public property - maps to this.#listStyle
+    set liststyle(val) {
+        this.#listStyle = val
+
+        // Update CSS variable for the list style
+        this.#list.style.setProperty('--list-style', val)
+    }
+    get liststyle() {
+        return this.#listStyle
+    }
+
+    // Getter/setter for the `data` public property - maps to this.#entries
+    set data(val) {
+        this.#entries = val
+
+        this.#list.innerHTML = ''
+        // Dynamically build the content of the ul/ol
+        Object.keys(this.#entries).forEach( (key, i) => {
+            // check if this.#entries is an array
+            if (Array.isArray(this.#entries)) {
+                this.#list.insertAdjacentHTML(
+                    'beforeend',
+                    `<li id="${this.id}-${key}" data-index="${i}">${this.#entries[key]}</li>`
+                )
+            } else {
+                // If the keyvalueseparator is 'NULL', then don't show the key
+                // otherwise, show the key followed by the separator
+                this.#list.insertAdjacentHTML(
+                    'beforeend',
+                    `<li id="${this.id}-${key}" data-index="${i}">${this.keyvalueseparator === 'NULL' ? '' : `${key}${this.keyvalueseparator}`}${this.#entries[key]}</li>`
+                )
+            }
+        })
+    }
+    get data() {
+        return this.#entries
+    }
+
+    /** Dynamically change/add list entries - rebuilds the list DOM element
+     * @param {string|number} key Object key or array index to change/add
+     * @param {string} val Updates/new list text (can be HTML)
+     * @example
+     * // Add a new entry to the list
+     * dataList.entry('newKey', 'New list item text')
+     * // Update an existing entry in the list
+     * dataList.entry('existingKey', 'Updated list item text')
+     */
+    entry(key, val) {
+        this.#entries[key] = val
+        this.data = this.#entries
+    }
+} // ---- end of Class ---- //
+
+// Make the class the default export so it can be used elsewhere
+export default DataList
+
+/** Self register the class to global
+ * Enables new data lists to be dynamically added via JS
+ * and lets the static methods be called
+ */
+window['DataList'] = DataList
+
+// Self-register the HTML tag
+customElements.define('data-list', DataList)
