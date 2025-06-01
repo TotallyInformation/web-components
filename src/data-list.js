a75bf60cfbd01052ceebb1572de727393e4cd19c
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2025-06-01 13:14:32 +0100

    Add light dom style loader

--- src/data-list.js
+++ src/data-list.js
@@ -26,22 +26,50 @@ import TiBaseComponent from '../libs/ti-base-component'
  *       topic (uib)
  */
 
-/** Only use a template if you want to isolate the code and CSS */
-const template = document.createElement('template')
-template.innerHTML = /*html*/`
-    <style>
-        :host {
-            display: block;   /* default is inline */
-            contain: content; /* performance boost */
+const styles = `
+        data-list {
             --list-style: disc; /* Default list style type */
+            --nested-indent: 40px; /* Default nested list indent - all browsers use 40px by default */
         }
-        ul {
-            list-style: var(--list-style, disc);
+        data-list ul {
+            list-style: var(--list-style);
         }
-    </style>
-    <slot></slot>
-    <ul></ul>
+        /* Nested list indentation */
+        data-list ul ul {
+            padding-inline-start: var(--nested-indent);
+            margin-inline-start: 0;
+        }
+        data-list .nested-container {
+            /* No list bullet on an li containing a nested lists unless the key text is shown */
+            list-style: none;
+        }
 `
+/** Only use a template if you want to isolate the code and CSS */
+// const template = document.createElement('template')
+// template.innerHTML = /*html*/`
+//     <style>
+//         :host {
+//             display: block;   /* default is inline */
+//             contain: content; /* performance boost */
+//             --list-style: disc; /* Default list style type */
+//             --nested-indent: 40px; /* Default nested list indent - all browsers use 40px by default */
+//         }
+//         ul {
+//             list-style: var(--list-style);
+//         }
+//         /* Nested list indentation */
+//         ul ul {
+//             padding-inline-start: var(--nested-indent);
+//             margin-inline-start: 0;
+//         }
+//         .nested-container {
+//             /* No list bullet on an li containing a nested lists unless the key text is shown */
+//             list-style: none;
+//         }
+//     </style>
+//     <slot></slot>
+//     <!-- <ul></ul> -->
+// `
 
 /** Namespace
  * @namespace Live
@@ -101,12 +129,16 @@ template.innerHTML = /*html*/`
  */
 class DataList extends TiBaseComponent {
     /** Component version */
-    static componentVersion = '2025-05-30'
+    static componentVersion = '2025-05-31'
 
-    #list = null // The list element
-    #listStyle = 'disc' // The default list style type
-    #entries = null // The entries in the list
-    #type = 'ul' // The type of list to use, ul or ol
+    /** The top-level list element - this is created in the shadow DOM */
+    #list = null
+    /** The list style type to use for the list - this is set as a CSS variable */
+    #listStyle = null
+    /** The data to use for the list - this is set via the `data` property */
+    #entries = null
+    /** The type of list to use, ul or ol @type {"ol"|"ul"} */
+    #type = 'ul'
 
     // Optional global variable name to use for the list data
     listvar = null
@@ -129,10 +161,9 @@ class DataList extends TiBaseComponent {
     constructor() {
         super()
         // Only attach the shadow dom if code and style isolation is needed - comment out if shadow dom not required
-        if (template && template.content) this._construct(template.content.cloneNode(true))
+        // if (template && template.content) this._construct(template.content.cloneNode(true))
 
-        // this.#list = document.createElement('ul')
-        this.#list = this.shadowRoot.querySelector('ul')
+        this.prependStylesheet(styles)
     }
 
     /** Runs when an instance is added to the DOM
@@ -189,6 +220,32 @@ class DataList extends TiBaseComponent {
         this._event('attribChanged', { attribute: attrib, newVal: newVal, oldVal: oldVal, })
     }
 
+    /** Updates the list attributes based on the current type and style
+     * @param {HTMLElement} listEl The list element to update
+     * @param {"ol"|"ul"} listType The type of list to use, either 'ol' for ordered or 'ul' for unordered
+     */
+    updateListAttributes(listEl, listType) {
+        if (listEl) {
+            if (listEl) {
+                // Update CSS classes/attributes instead of recreating element
+                listEl.setAttribute('data-list-type', listType)
+
+                // Update ARIA attributes for accessibility
+                if (listType === 'ol') {
+                    this.listStyle = 'decimal'
+                    listEl.setAttribute('aria-label', 'Ordered list')
+                } else {
+                    this.listStyle = 'disc'
+                    listEl.setAttribute('aria-label', 'Unordered list')
+                }
+            }
+            if (this.#listStyle) {
+                // Update the CSS variable for the list style
+                listEl.style.setProperty('--list-style', this.#listStyle)
+            }
+        }
+    }
+
     // Getter/setter for the `type` public property - maps to this.#type
     set type(val) {
         if (!['ul', 'ol'].includes(val)) {
@@ -198,17 +255,7 @@ class DataList extends TiBaseComponent {
 
         this.#type = val
 
-        // Update CSS classes/attributes instead of recreating element
-        this.#list.setAttribute('data-list-type', val)
-
-        // Update ARIA attributes for accessibility
-        if (val === 'ol') {
-            this.listStyle = 'decimal'
-            this.#list.setAttribute('aria-label', 'Ordered list')
-        } else {
-            this.listStyle = 'disc'
-            this.#list.setAttribute('aria-label', 'Unordered list')
-        }
+        this.updateListAttributes(this.#list, this.#type)
     }
     get type() {
         return this.#type
@@ -217,9 +264,7 @@ class DataList extends TiBaseComponent {
     // Getter/setter for the `listStyle` public property - maps to this.#listStyle
     set liststyle(val) {
         this.#listStyle = val
-
-        // Update CSS variable for the list style
-        this.#list.style.setProperty('--list-style', val)
+        this.updateListAttributes(this.#list, this.#type)
     }
     get liststyle() {
         return this.#listStyle
@@ -229,24 +274,18 @@ class DataList extends TiBaseComponent {
     set data(val) {
         this.#entries = val
 
-        this.#list.innerHTML = ''
+        // If the list already exists, delete it
+        if (this.#list) this.#list.remove()
+
         // Dynamically build the content of the ul/ol
-        Object.keys(this.#entries).forEach( (key, i) => {
-            // check if this.#entries is an array
-            if (Array.isArray(this.#entries)) {
-                this.#list.insertAdjacentHTML(
-                    'beforeend',
-                    `<li id="${this.id}-${key}" data-index="${i}">${this.#entries[key]}</li>`
-                )
-            } else {
-                // If the keyvalueseparator is 'NULL', then don't show the key
-                // otherwise, show the key followed by the separator
-                this.#list.insertAdjacentHTML(
-                    'beforeend',
-                    `<li id="${this.id}-${key}" data-index="${i}">${this.keyvalueseparator === 'NULL' ? '' : `${key}${this.keyvalueseparator}`}${this.#entries[key]}</li>`
-                )
-            }
-        })
+        // Object.keys(this.#entries).forEach( this.buildListItem.bind(this))
+        this.buildList(this.#entries, this, this.#type, 1)
+
+        this.#list = this.querySelector('ul')
+        this.updateListAttributes(this.#list, this.#type)
+
+        // Issue a custom event to notify that the data has changed
+        this._event('dataChanged', this.#entries)
     }
     get data() {
         return this.#entries
@@ -264,6 +303,91 @@ class DataList extends TiBaseComponent {
     entry(key, val) {
         this.#entries[key] = val
         this.data = this.#entries
+    }
+
+    /** Builds a list from input data
+     * @param {Array|object} listData Source data for the list, can be an array or an object
+     * @param {HTMLElement|ShadowRoot} parentEl The parent element to append the list to
+     * @param {"ol"|"ul"} type  The type of list to create, either 'ol' for ordered or 'ul' for unordered
+     * @param {number} depth  Recursion depth. Defaults to 1. Used to limit recursion depth for nested objects/arrays.
+     */
+    buildList(listData, parentEl, type, depth) {
+        if (depth === undefined) depth = 1
+        // console.log(`[DataList] ${this.id} buildList: listData=${listData}, listEl=${parentEl}, depth=${depth}`)
+
+        // Create a new list element (all lists are created as ul, numbering is done via CSS)
+        const listEl = document.createElement('ul')
+        if (depth > 1) {
+            // If this is a nested list, add a class to the list element
+            listEl.classList.add('nested-list')
+            // and add a data-depth attribute to the list element
+            listEl.setAttribute('data-depth', depth.toString())
+        }
+
+        Object.entries(listData).forEach(([key, value], i) => {
+            // Is the key a number? Keep a record
+            const keyIsNumeric = !isNaN(parseInt(key))
+
+            // Is the value an object or array?
+            if (typeof value === 'object' && value !== null) {
+                if (depth > 3) {
+                    // If depth is too high, just show the key and value as a string
+                    listEl.insertAdjacentHTML(
+                        'beforeend',
+                        this.buildLIhtml({ arrayType: Array.isArray(listData), key, value: JSON.stringify(value), i, })
+                    )
+                    console.warn(`[DataList] ${this.id} buildList: Depth limit reached for key "${key}", showing as string.`)
+                    return
+                }
+
+                // Create a new list item for the key
+                const li = document.createElement('li')
+
+                // If key is not numeric and if this.keyvalueseparator is not 'NULL', add key as text
+                if (!keyIsNumeric && this.keyvalueseparator !== 'NULL') {
+                    const keySpan = document.createElement('span')
+                    keySpan.textContent = `${key}${this.keyvalueseparator}`
+                    // keySpan.classList.add('nested-key')
+                    li.appendChild(keySpan)
+                } else {
+                    li.classList.add('nested-container')
+                }
+
+                // recurse to create a nested list
+                this.buildList(value, li, type, ++depth)
+                this.updateListAttributes(li, type)
+                // add the new list item to its parent element
+                listEl.appendChild(li)
+            } else {
+                // Otherwise, just create a list item with the key and value
+                // console.log(`[DataList] ${this.id} buildList: key=${key}, value=${value}, i=${i}`)
+                listEl.insertAdjacentHTML(
+                    'beforeend',
+                    this.buildLIhtml({ arrayType: Array.isArray(listData), key, value, i, })
+                )
+            }
+        })
+
+        // Attach the list as the last child of the parent element
+        parentEl.appendChild(listEl)
+    }
+
+    /** Builds a list item HTML string based on the provided options.
+     * @param {object} options Object containing options for building the list item
+     * @param {boolean} options.arrayType True if the list is an array, false if it is an object
+     * @param {string} options.key The key for the list item, used as the id and data-index
+     * @param {string} options.value The value for the list item, displayed as the text
+     * @param {number} options.i The index of the item in the list, used for data-index attribute
+     * @returns {string} The HTML string for the list item
+     */
+    buildLIhtml({ arrayType, key, value, i, }) {
+        // check if this.#entries is an array
+        if (arrayType) {
+            return `<li id="${this.id}-${key}" data-index="${i}">${value}</li>`
+        }
+        // If the keyvalueseparator is 'NULL', then don't show the key
+        // otherwise, show the key followed by the separator
+        return `<li id="${this.id}-${key}" data-index="${i}">${this.keyvalueseparator === 'NULL' ? '' : `${key}${this.keyvalueseparator}`}${value}</li>`
     }
 } // ---- end of Class ---- //
 
